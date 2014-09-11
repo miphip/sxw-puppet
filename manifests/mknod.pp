15f1c9c3a51d0125d3c393e824aabedd7c4cc4e0
mikaelpa <mikael.pahmp@schneider-electric.com>
2014-09-11 12:57:10 +0200

    mkdir in sxw::mknod.

--- manifests/mknod.pp
+++ manifests/mknod.pp
@@ -1,6 +1,10 @@
 class sxw::mknod {
+  file { '/opt/tac/bin':
+    ensure => directory,
+  }
   file { '/opt/tac/bin/mknod_cvt_vtl':
-    ensure => file,
-    source => 'puppet:///modules/sxw/mknod_cvt_vtl',
+    ensure  => file,
+    source  => 'puppet:///modules/sxw/mknod_cvt_vtl',
+    require => File['/opt/tac/bin'],
   }
 }

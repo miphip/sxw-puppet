146a94ed9d0ce9430400b605b8354d4336db7e7c
mikaelpa <mikael.pahmp@schneider-electric.com>
2014-09-11 13:02:19 +0200

    mkdir recursively in sxw::mknod.

--- manifests/mknod.pp
+++ manifests/mknod.pp
@@ -1,10 +1,12 @@
 class sxw::mknod {
+  file { '/opt/tac':
+    ensure => directory,
+  }
   file { '/opt/tac/bin':
     ensure => directory,
   }
   file { '/opt/tac/bin/mknod_cvt_vtl':
     ensure  => file,
     source  => 'puppet:///modules/sxw/mknod_cvt_vtl',
-    require => File['/opt/tac/bin'],
   }
 }
